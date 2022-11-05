61644a7c8a2c3a1035d078fbd85f8219a432fd1f
yaana <ya.anastasya.b@yandex.ru>
2022-11-05 10:51:19 +0300

    fix: время отображения короткометражек без указания часов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -6,16 +6,17 @@ import deleteButton from "../../images/film-delete-btn.svg";
 
 function MoviesCard({ card, handleMovieButtonClick, isMovieJS, handleMovieDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
+  const duration = card.duration >= 60 ? `${Math.floor(card.duration / 60)}ч ${card.duration % 60}м` : `${card.duration} м`;
   const isSaved = card.owner === currentUser._id;
   const buttonImage = (card.owner ? saveButtonActive : saveButtonInactive);
-  const cardImage = (card.image.url ? (`https://api.nomoreparties.co${card.image.url}`) : card.image)
+  const cardImage = (card.image.url ? (`https://api.nomoreparties.co${card.image.url}`) : card.image);
 
   return (
     <article className="movies-card">
       <div className="movies-card__info">
         <div className="movies-card__descriprion">
           <h2 className="movies-card__title">{card.nameRU}</h2>
-          <p className="movies-card__duration">{`${Math.floor(card.duration / 60)}ч ${card.duration % 60}м`}</p>
+          <p className="movies-card__duration">{duration}</p>
         </div>
         {isMovieJS &&
           <button aria-label="Сохранить фильм" type="button" className="movies-card__button">
